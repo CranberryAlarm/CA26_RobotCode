85dd6647562fcdb9e749fe7e2db817f2de122208
dracco1993 <dracco1993@gmail.com>
2025-12-24 00:34:57 -0500

    Fix drivetrain dep issues

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -6,9 +6,9 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
@@ -19,17 +19,25 @@ public class DriveSubsystem extends SubsystemBase {
   private final SparkMax m_frontRight = new SparkMax(DriveConstants.kFrontRightMotorId, MotorType.kBrushless);
   private final SparkMax m_backRight = new SparkMax(DriveConstants.kBackRightMotorId, MotorType.kBrushless);
 
-  // Motor controller groups for left and right sides
-  private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_frontLeft, m_backLeft);
-  private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_frontRight, m_backRight);
-
   // Differential drive object
-  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_frontLeft, m_frontRight);
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
+    // Configure back motors to follow front motors
+    var backLeftConfig = new SparkMaxConfig().follow(DriveConstants.kFrontLeftMotorId);
+    m_backLeft.configure(backLeftConfig, SparkMax.ResetMode.kResetSafeParameters,
+        SparkMax.PersistMode.kPersistParameters);
+
+    var backRightConfig = new SparkMaxConfig().follow(DriveConstants.kFrontRightMotorId);
+    m_backRight.configure(backRightConfig, SparkMax.ResetMode.kResetSafeParameters,
+        SparkMax.PersistMode.kPersistParameters);
+
     // Invert right side motors if needed
-    // m_rightMotors.setInverted(true);
+    // SparkMaxConfig rightConfig = new SparkMaxConfig();
+    // rightConfig.inverted(true);
+    // m_frontRight.configure(rightConfig, SparkMax.ResetMode.kResetSafeParameters,
+    // SparkMax.PersistMode.kPersistParameters);
   }
 
   /**
