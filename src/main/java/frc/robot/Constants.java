dce476662bdb959f9512b62527bc10d60c149edd
dracco1993 <dracco1993@gmail.com>
2026-01-03 16:46:00 -0500

    Update swerve constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,17 +6,29 @@ public final class Constants {
   }
 
   public static class DriveConstants {
-    public static final int kFLTMotorId = 5;
-    public static final int kFLDMotorId = 6;
+    public static class FL {
+      public static final int kTurnMotorId = 5;
+      public static final int kDriveMotorId = 6;
+      public static final int kAbsId = 0;
+    }
 
-    public static final int kBLTMotorId = 7;
-    public static final int kBLDMotorId = 8;
+    public static class FR {
+      public static final int kTurnMotorId = 9;
+      public static final int kDriveMotorId = 10;
+      public static final int kAbsId = 2;
+    }
 
-    public static final int kFRTMotorId = 9;
-    public static final int kFRDMotorId = 10;
+    public static class BL {
+      public static final int kTurnMotorId = 7;
+      public static final int kDriveMotorId = 8;
+      public static final int kAbsId = 1;
+    }
 
-    public static final int kBRTMotorId = 11;
-    public static final int kBRDMotorId = 12;
+    public static class BR {
+      public static final int kTurnMotorId = 11;
+      public static final int kDriveMotorId = 12;
+      public static final int kAbsId = 3;
+    }
   }
 
   public static class AlgaeConstants {
