41597d91a12e332a294d83d5d678f72bb6538160
Jordan Miller <dracco1993@gmail.com>
2026-01-11 18:43:47 -0500

    YAMS-ify it, baby!

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,56 +3,75 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private final SparkFlex flywheelMotor;
 
-  private final double INTAKE_SPEED = 0.5;
+  private static final double INTAKE_SPEED = 0.5;
+
+  // SparkFlex controlling the intake flywheel
+  private SparkFlex flywheelSpark = new SparkFlex(Constants.IntakeConstants.kFlywheelMotorId, MotorType.kBrushless);
+
+  private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+      .withControlMode(ControlMode.OPEN_LOOP)
+      .withTelemetry("IntakeMotor", TelemetryVerbosity.HIGH)
+      .withGearing(new MechanismGearing(GearBox.fromReductionStages(1))) // Direct drive, adjust if geared
+      .withMotorInverted(false)
+      .withIdleMode(MotorMode.COAST)
+      .withStatorCurrentLimit(Amps.of(40));
+
+  private SmartMotorController smc = new SparkWrapper(flywheelSpark, DCMotor.getNeoVortex(1), smcConfig);
+
+  private final FlyWheelConfig intakeConfig = new FlyWheelConfig(smc)
+      .withDiameter(Inches.of(4))
+      .withMass(Pounds.of(0.5))
+      .withUpperSoftLimit(RPM.of(6000))
+      .withLowerSoftLimit(RPM.of(-6000))
+      .withTelemetry("Intake", TelemetryVerbosity.HIGH);
+
+  private FlyWheel intake = new FlyWheel(intakeConfig);
 
   public IntakeSubsystem() {
-    flywheelMotor = new SparkFlex(Constants.IntakeConstants.kFlywheelMotorId, MotorType.kBrushless);
-  }
-
-  /**
-   * Run the intake to pull game pieces in.
-   */
-  public void intake() {
-    flywheelMotor.set(INTAKE_SPEED);
-  }
-
-  /**
-   * Run the intake in reverse to eject game pieces.
-   */
-  public void eject() {
-    flywheelMotor.set(-INTAKE_SPEED);
-  }
-
-  /**
-   * Stop the intake motor.
-   */
-  public void stop() {
-    flywheelMotor.set(0);
   }
 
   /**
    * Command to run the intake while held.
    */
   public Command intakeCommand() {
-    return this.runEnd(this::intake, this::stop).withName("Intake.Run");
+    return intake.set(INTAKE_SPEED).finallyDo(() -> smc.setDutyCycle(0)).withName("Intake.Run");
   }
 
   /**
    * Command to eject while held.
    */
   public Command ejectCommand() {
-    return this.runEnd(this::eject, this::stop).withName("Intake.Eject");
+    return intake.set(-INTAKE_SPEED).finallyDo(() -> smc.setDutyCycle(0)).withName("Intake.Eject");
   }
 
   @Override
   public void periodic() {
-    // Add any periodic logging here if needed
+    intake.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    intake.simIterate();
   }
 }
