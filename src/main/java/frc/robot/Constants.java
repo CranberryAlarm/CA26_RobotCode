4272861e05bef2d42d526070e107b2e1af00ce44
dracco1993 <dracco1993@gmail.com>
2026-01-28 12:31:23 -0500

    Remove BottomFeeder code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -79,18 +79,6 @@ public final class Constants {
     }
   }
 
-  public static class AlgaeConstants {
-    public static final int kWristMotorId = 13;
-    public static final int kIntakeMotorId = 14;
-  }
-
-  public static class CoralConstants {
-    public static final int kLeftIndexMotorId = 11;
-    public static final int kRightIndexMotorId = 12;
-
-    public static final int kIndexLaserCANId = 0;
-  }
-
   public static class ShooterConstants {
     // 2 Neos, 4in shooter wheels
     public static final int kLeaderMotorId = 15;
